82f19ea0e0ede19065bc390625d8fc587784f78b
Subhendu Ranjan Mishra <850012+subhrm@users.noreply.github.com>
2023-10-05 18:22:55 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Learning and Exploring `manim` Library 
+
+[Manim Documentation](https://docs.manim.community/en/stable/)
---
+++ circle.py
@@ -0,0 +1,9 @@
+from manim import *
+
+""" run `manim -pql circle.py CreateCircle`  to generate video"""
+
+class CreateCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
+        self.play(Create(circle))  # show the circle on screen
